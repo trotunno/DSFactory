5145c31d6cdfab6ab04e29e94703c545114a4ffe
trotunno <thomasdylanrotunno@gmail.com>
2022-06-04 14:07:38 -0500

    added dsfactory files... whoops

---
+++ dsfactory/dsfactory.c
@@ -0,0 +1,8 @@
+/*
+ * dsfactory.c
+ *
+ *  Created on: Jun 4, 2022
+ *      Author: Dylan
+ */
+
+#include "dsfactory.h"
---
+++ dsfactory/dsfactory.h
@@ -0,0 +1,15 @@
+/*
+ * dsfactory.h
+ *
+ *  Created on: Jun 4, 2022
+ *      Author: Dylan
+ */
+
+#ifndef _DSFACTORY_DSFACTORY_H
+#define _DSFACTORY_DSFACTORY_H
+
+#include "../ringbuffer/ringbuffer.h"
+#include "../queue/queue.h"
+#include "../stack/stack.h"
+
+#endif
